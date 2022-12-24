6f987eb1c0939060b56bd3ae55ba5558e007362a
choilettuce <tkdgnstkdcn@naver.com>
2022-12-24 23:26:16 +0900

    feat axios

--- client/movie-app/src/components/login/login.js
+++ client/movie-app/src/components/login/login.js
@@ -24,8 +24,8 @@ function Login() {
         axios.post('https://localhost:8000')
         .then(res => {
             console.log(res)
-            console.log('res.data.userId :: ', res.data.userId)
-            console.log('res.data.pw :: ', res.data.pw) })
+            console.log('res.data.userId : ', res.data.userId)
+            console.log('res.data.pw : ', res.data.pw) })
         .catch()
     },
     // 페이지 호출 후 처음 한번만 호출될 수 있도록 [] 추가
